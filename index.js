192667469458b8ed4ed6e5da4bbefb3a127d9b0a
Niraxjana <niranjanabofficial@gmail.com>
2025-12-07 17:47:40 +0530

    first commit

--- index.js
+++ index.js
@@ -2,24 +2,30 @@ import express from 'express';
 import mongoose from 'mongoose';
 import Product from './models/product.model.js';
 import productRoute from './routes/product.route.js';
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 //middleware
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://niranjanauser:admin2005@backenddb.nyxloo5.mongodb.net/BackendDb"
-)
-.then(() => {
-  console.log("Connected to MongoDB");
 
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+
+    app.listen(3000, () => {
+      console.log("🚀 Server running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err.message);
   });
-})
-.catch((err) => {
-  console.error("Error connecting to MongoDB:", err);
-});
 
+
+
+
+app.use(express.static("public"));
 
 //routes
 app.use("/api/products", productRoute);
